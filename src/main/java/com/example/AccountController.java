6d5325d44fd41208330109cb697e0a7eaeda17ee
Minins <vitalijs.minins@accenture.com>
2017-11-16 10:52:55 +0200

    aa

--- src/main/java/com/example/AccountController.java
+++ src/main/java/com/example/AccountController.java
@@ -5,9 +5,7 @@ import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Set;
 
-import com.example.dao.AccountRRepository;
 import com.example.dao.AccountRepository;
 import com.example.model.Account;
 import com.example.model.AccountRel;
@@ -16,10 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,11 +25,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AccountController {
     @Autowired
-    AccountRRepository repository;
+    AccountRepository repository;
 
     @RequestMapping(path = "/accounts",method = RequestMethod.GET)
-    public Iterable<AccountRel> getAccounts() {
-        Iterable<AccountRel> accounts = repository.findAll();
+    public Iterable<Account> getAccounts() {
+        Iterable<Account> accounts = repository.findAll();
         return accounts;
     }
 
